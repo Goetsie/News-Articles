baf62ec137ca241ae8bc3db8fdb5338fd8796cca
Arno <r0741327@student.thomasmore.be>
2020-11-13 10:03:03 +0100

    Small changes to create & update article

--- src/app/admin/to-review/to-review.component.ts
+++ src/app/admin/to-review/to-review.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { map, tap } from 'rxjs/operators';
+import { ArticleService } from 'src/app/articles/article.service';
+import { Article } from 'src/app/articles/models/article.model';
 
 @Component({
   selector: 'app-to-review',
@@ -7,7 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToReviewComponent implements OnInit {
 
-  constructor() { }
+  articles: Article[];
+
+  dataSource;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  displayedColumns: string[] = ['title', 'subTitle', 'user.name', 'actions'];
+
+  constructor(private _articleService: ArticleService) { 
+    this._articleService.getArticles(0)
+    .pipe(
+      map(articles => articles.filter(article => article.articleStatusID == 2)), // Only get the article who needs a review
+      tap(t => console.log("My articles:", t))
+    )
+    .subscribe(
+    result => {
+      if (result.length == 0){
+        this.articles = null;
+      }else{
+        this.articles = result;
+      }
+      
+      this.dataSource = new MatTableDataSource(this.articles);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+
+    console.log("My articles:", this.articles);
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log("Filter value:", filterValue);
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 
   ngOnInit(): void {
   }
