3f16d3c013bc9bf283c85c8fcf77aa8e333e7ae3
Arno <r0741327@student.thomasmore.be>
2020-11-17 23:25:06 +0100

    some layout changes

--- src/app/articles/article-detail/article-detail.component.ts
+++ src/app/articles/article-detail/article-detail.component.ts
@@ -14,7 +14,7 @@ import { AuthenticateService } from 'src/app/security/services/authenticate.serv
 })
 export class ArticleDetailComponent implements OnInit {
 
-  articleID: number = null; 
+  articleID: number = null;
   public article: Article;
 
   alineas: String[];
@@ -24,7 +24,7 @@ export class ArticleDetailComponent implements OnInit {
   constructor(private _articleService: ArticleService, private route: ActivatedRoute, private _authenticateService: AuthenticateService) {
     this.articleID = parseInt(this.route.snapshot.paramMap.get('id'));
     console.log("ArticleID in detail constructor:", this.articleID);
-   }
+  }
 
   ngOnInit() {
     // this.articleID = parseInt(this.route.snapshot.paramMap.get('id'));
@@ -40,6 +40,10 @@ export class ArticleDetailComponent implements OnInit {
           // Article body needs to we splitted by the new line characters, otherwise one (long) text.
           this.alineas = this.article.body.split(/\r?\n/);
         });
+  }
+
+  like(id: number){
+    console.log("User likes this article");
   }
 
 }
