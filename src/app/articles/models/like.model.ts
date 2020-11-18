e5dc59c5209c1c9a73667ba0299012aee6ea3106
Arno <r0741327@student.thomasmore.be>
2020-11-18 11:41:02 +0100

    Like service & model

---
+++ src/app/articles/models/like.model.ts
@@ -0,0 +1,9 @@
+export class Like {
+    constructor(
+        public likeID: number, 
+        public userID: number, 
+        public articleID: number, 
+        // public article?: Article,
+        // public user?: User
+        ){ }
+}
---
+++ src/app/articles/services/like.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Like } from '../models/like.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LikeService {
+
+  public likeList: Array<Like> = [];
+
+  constructor(private _httpClient: HttpClient) { }
+
+
+  // GET --> get all likes
+  getLikes(): Observable<Like[]> {
+      return this._httpClient.get<Like[]>("https://localhost:44348/api/Like");
+      // NEED TO GET LIKES PER ARTICLE AND PER USER
+      // return this._httpClient.get<Like[]>("https://localhost:44348/api/Like?articleID=" + articleID.toString());
+  }
+
+  // POST --> post a new Like on an article
+  addLike(like: Like) {
+    console.log("User with id:", like.userID, "likes article with id:", like.articleID);
+    return this._httpClient.post<Like>("https://localhost:44348/api/Like", like);
+  }
+
+  // DELETE --> delete the like of a user
+  deleteLike(likeID: number) {
+    return this._httpClient.delete<Like>("https://localhost:44348/api/Like/" + likeID.toString());
+  }
+
+}
